15913c85294aa59350af7c3fff100b921f1a9fca
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-06-03 13:15:19 -0400

    initial setup

---
+++ server/app.js
@@ -0,0 +1,17 @@
+// import dotenv from "dotenv";
+// import express from "express";
+// import bodyParser from "body-parser";
+// Import OpenAI from “openai”
+//
+// dotenv.config(); // Load the .env file
+//
+// const app = express();
+// const port = 5001;
+//
+// // use middleware to parse json request bodies
+// app.use(bodyParser.json());
+//
+//
+// app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
